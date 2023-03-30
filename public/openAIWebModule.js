cc83f3b8b4adb1b15bbd50810b177ed666e8c085
leviwheeling <129359504+leviwheeling@users.noreply.github.com>
2023-03-30 13:24:43 -0500

    Update openAIWebModule.js

--- public/openAIWebModule.js
+++ public/openAIWebModule.js
@@ -1,5 +1,5 @@
 // Filename: public/openAIWebModule.js
-import {generateText} from 'backend/openAI.jsw';
+import {generateText} from './backend/openAI.jsw';
 
 window.getGeneratedText = function(prompt) {
   return generateText(prompt);
