6cccc90fe4d65cc36ea2913e66aba621ef05e18d
leviwheeling <129359504+leviwheeling@users.noreply.github.com>
2023-03-30 14:36:54 -0500

    Update openAIWebModuleClient.js

--- public/openAIWebModuleClient.js
+++ public/openAIWebModuleClient.js
@@ -1,6 +1,6 @@
 // Filename: public/openAIWebModuleClient.js
 async function getGeneratedText(prompt) {
-   const result = await fetch("https://leviwheeling.github.io/backend/generateText", {
+   const result = await fetch("https://leviwheeling.github.io/backend/generateText.js", {
     method: "POST",
     headers: {
         "Content-Type": "application/json",
