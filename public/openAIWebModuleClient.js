a0de1b60f64bd89cc75b1e4573a600bef95fee1e
leviwheeling <129359504+leviwheeling@users.noreply.github.com>
2023-04-03 08:53:04 -0500

    Update openAIWebModuleClient.js

--- public/openAIWebModuleClient.js
+++ public/openAIWebModuleClient.js
@@ -1,6 +1,6 @@
 // Filename: public/openAIWebModuleClient.js
 async function getGeneratedText(prompt) {
-  const result = await fetch("https://openapi-14oxz8ev5-leviwheeling.vercel.app/", {
+  const result = await fetch("https://openapi-jdnxe03jj-leviwheeling.vercel.app/api.js", {
     method: "POST",
     mode: "cors",
     headers: {
